56f41a573fbae112fd83fae5bee9a657ec7b93ca
XizheYin-NJU <62123683+XizheYin-NJU@users.noreply.github.com>
2023-07-05 15:33:56 +0800

    Create r6.rs

---
+++ r6.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut _local7 = xi_core_lib::line_cache_shadow::Builder::new();
+    let _ = xi_core_lib::line_cache_shadow::Builder::add_span(
+        &mut (_local7),
+        103444964239,
+        103446848117951,
+        191,
+    );
+    let _ = xi_core_lib::line_cache_shadow::Builder::add_span(
+        &mut (_local7),
+        10344644715844964239,
+        10344644715848117951,
+        191,
+    );
+    let _ = xi_core_lib::line_cache_shadow::Builder::add_span(
+        &mut (_local7),
+        103444964239,
+        10344644715848117951,
+        191,
+    );
+    let _ = xi_core_lib::line_cache_shadow::Builder::add_span(
+        &mut (_local7),
+        13816973012072644543,
+        13816973012072644543,
+        191,
+    );
+}
