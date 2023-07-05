39f5c1d71047f343862aa168979fdffe4f8c79ea
XizheYin-NJU <62123683+XizheYin-NJU@users.noreply.github.com>
2023-07-05 15:32:24 +0800

    Create r1.rs

---
+++ r1.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut _local6 = xi_core_lib::line_cache_shadow::RenderPlan::create(
+        13238251629574210756,
+        4,
+        51510762828464068,
+    );
+
+    let _ = xi_core_lib::line_cache_shadow::RenderPlan::request_lines(
+        &mut (_local6),
+        1125972401308826,
+        14192813596667262903,
+    );
+    let _ = xi_core_lib::annotations::AnnotationStore::new();
+    let _ = xi_core_lib::line_cache_shadow::RenderPlan::request_lines(
+        &mut (_local6),
+        13238251629368031159,
+        13238250843596178433,
+    );
+}
