3d38538c159581ad78c8857b352b2c82fe403b62
XizheYin-NJU <62123683+XizheYin-NJU@users.noreply.github.com>
2023-07-05 15:34:20 +0800

    Create r8.rs

---
+++ r8.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut _local7 = xi_core_lib::line_cache_shadow::RenderPlan::create(
+        3182967604875373612,
+        3182968324947694547,
+        15263776468834177892,
+    );
+}
