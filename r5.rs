8ad224b33287c4092fec779d1686db493f33f140
XizheYin-NJU <62123683+XizheYin-NJU@users.noreply.github.com>
2023-07-05 15:33:41 +0800

    Create r5.rs

---
+++ r5.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut _local7 = xi_core_lib::line_cache_shadow::Builder::new();
+    let _ = xi_core_lib::line_cache_shadow::Builder::add_span(
+        &mut (_local7),
+        18446582924227601774,
+        7232622012058070883,
+        0,
+    );
+    let _ = xi_core_lib::line_cache_shadow::Builder::add_span(
+        &mut (_local7),
+        9251922454743391839,
+        8027208270804645229,
+        0,
+    );
+}
